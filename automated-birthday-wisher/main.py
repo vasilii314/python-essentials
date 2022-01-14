4e9e0cc8b1a15d739774581e34f4bbb3c1942d40
vasilii314 <pythagoras3145926@gmail.com>
2022-01-14 19:27:34 +0400

    14 Jan 2022

---
+++ automated-birthday-wisher/main.py
@@ -0,0 +1,27 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+MY_EMAIL = "your@emai.com"
+MY_PASSWORD = "your_password"
+
+today = (dt.datetime.now().month, dt.datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read().replace("[NAME]", birthday_person["name"])
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}")
+
